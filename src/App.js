e8f2033db5d0127e98e563b5568bb8a9f689dd6c
LeeJoEun-01 <joeun1005@naver.com>
2022-03-24 13:19:22 +0900

    Create Example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Foo extends React.Component  {
+  componentDidMount() {
+    console.log("Foo componentDidMount");
+  }
+  componentWillUnmount() {
+    console.log("Foo componentWillUnmount");
+  }
+
+  //라이프 사이클 훅
+  static getDerivedStateFromProps(nextProps, prevProps) {
+    console.log("Foo getDerivedStateFromProps", nextProps, prevProps);
+    return {}; // return 꼭 해줘야 됨
+  }
+  render() {
+    console.log("Foo render");
+    return <p>Foo</p>;
+  } 
+}
+class App extends React.Component {
+  // 엘리먼트의 타입이 다른 경우
+  state = {
+    count: 0,
+  };
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({count: this.state.count + 1});
+    }, 1000);
+  }
+
+  render() {
+    if (this.state.count % 2 === 0) {
+      return <Foo name="Mark" />;
+    }
+
+    return <Foo name="Hanna" />;
+  }
 }
 
 export default App;
